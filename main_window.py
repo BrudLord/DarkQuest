99073c84dc3e6afab4353a3848ebefffb2b5e106
BlaBlaaBla <miderost@mail.ru>
2021-04-11 21:48:33 +0300

    acscacsaca

--- main_window.py
+++ main_window.py
@@ -9,7 +9,7 @@ def main_menu():
     return '''<!DOCTYPE html>
     <head>
     <style>
-    body {background-color: #2B2517;}
+    body {background-color: #2B2517; background-size: cover;}
     </style>
     </head>
     <body background="https://steamuserimages-a.akamaihd.net/ugc/253716559962314766/4C9C8BAA47DD8DBFE20E5A6243D1E7B41104BB87/" width="100%" height="100%">
