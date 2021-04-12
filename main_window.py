44a56f16df52ac1ef5b5b757342d4af13773cc04
BlaBlaaBla <miderost@mail.ru>
2021-04-12 20:53:32 +0300

    забацал помощь и кнопки

--- main_window.py
+++ main_window.py
@@ -1,22 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/countdown')
-def main_menu():
-    return '''<!DOCTYPE html>
-    <head>
-    <style>
-    body {background-color: #2B2517; background-size: cover;}
-    </style>
-    </head>
-    <body background="https://steamuserimages-a.akamaihd.net/ugc/253716559962314766/4C9C8BAA47DD8DBFE20E5A6243D1E7B41104BB87/" width="100%" height="100%">
-    <input type="button" value=" Нажми меня нежно ">
-    </body>
-    </html>
-    '''
+def main_window():
+    return render_template('main_window.html')
+@app.route('/help')
+def help():
+    return render_template('help.html')
 
 
 
